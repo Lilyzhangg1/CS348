b9d80b35652d27eddb6cdcd5af62e874a6c1bef0
Bokang Keith Mogomotsi <bokangmg24@gmail.com>
2025-07-06 20:57:55 -0400

    friends so far

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.routers import auth, restaurants, wishlist, ratings, top_rated_weekly, friends
+from backend.routers import auth, restaurants, wishlist, ratings, top_rated_weekly, friends, users
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -16,3 +16,4 @@ app.include_router(wishlist.router, prefix="/wishlist", tags=["wishlist"])
 app.include_router(ratings.router, prefix="/rating",tags=["rating"])
 app.include_router(top_rated_weekly.router, prefix="/top-rated-weekly", tags=["top-rated-weekly"])
 app.include_router(friends.router, prefix="/friends", tags=["friends"])
+app.include_router(users.router, prefix="/users", tags=["users"])
