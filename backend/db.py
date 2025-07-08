16e9487efe22933480b30952464a7f6f6ab8a275
Mikayla Mao <m23mao@uwaterloo.ca>
2025-07-08 00:04:07 -0400

    db

--- backend/db.py
+++ backend/db.py
@@ -1,8 +1,7 @@
 import sqlite3
 
 #volime mounted on railway
-# DB_PATH = "/data/waterlooeats.db"
-DB_PATH = "production/waterlooeats.db"
+DB_PATH = "/data/waterlooeats.db"
 
 def get_db():
     conn = sqlite3.connect(DB_PATH)
